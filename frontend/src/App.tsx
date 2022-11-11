0281f319fce8ad21a48f923c49671adca9e7004f
bh2980 <bhwk2980@gmail.com>
2022-11-11 21:16:35 +0900

    test: emotion 테스트

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import React from 'react';
 
+const testCSS = () => css`
+	background-color: black;
+	color: red;
+`;
+
 function App() {
-  return (
-    <div className="App">
-      hello world!
-    </div>
-  );
+	return (
+		<div css={testCSS} className="App">
+			hello world!
+		</div>
+	);
 }
 
 export default App;
