c292a2e73eb5ac622ac380a873ed80225e3b4ba8
bh2980 <bhwk2980@gmail.com>
2022-11-12 03:14:12 +0900

    chore: @svgr/webpack 제거

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -15,13 +15,4 @@ module.exports = {
 	babel: {
 		presets: ['@emotion/babel-preset-css-prop'],
 	},
-	webpack: {
-		configure: (config, { env, paths }) => {
-			config.module.rules.unshift({
-				test: /\.svg$/,
-				use: ['@svgr/webpack'],
-			});
-			return config;
-		},
-	},
 };
