106e87965190568748530261247bc922c329ceea
bh2980 <bhwk2980@gmail.com>
2022-11-14 20:25:55 +0900

    chore: 크락코 alias 에러 fix

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -6,7 +6,7 @@ module.exports = {
 			plugin: CracoAlias,
 			options: {
 				source: 'tsconfig',
-				tsConfigPath: 'tsconfig.paths.json',
+				tsConfigPath: 'tsconfig.paths.json'
 			},
 		},
 	],
