29bbb3a3d6ae8450b799ecc32807ad88ac44f5ec
AnJeongMin <ajhappy12@gmail.com>
2022-11-14 20:27:51 +0900

    chore: swagger 설정

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,2 +1,3 @@
 export * from './env.config';
 export * from './typeorm.config';
+export * from './swagger.config';
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from '@config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	setupSwagger(app);
+	await app.listen(8080);
 }
 bootstrap();
