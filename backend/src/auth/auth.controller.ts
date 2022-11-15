f2d21c160c20be25f16134c0a0972dc54549eca1
AnJeongMin <ajhappy12@gmail.com>
2022-11-15 14:03:18 +0900

    chore: BE - user와 auth 모듈 생성

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+	constructor() {}
+}
---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('user')
+export class UserController {
+	constructor(private readonly userService: UserService) {}
+}
