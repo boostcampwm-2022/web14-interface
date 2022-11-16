27e8628489828a3b2826f6e84080f795027e96c5
bh2980 <bhwk2980@gmail.com>
2022-11-17 03:05:26 +0900

    test: FBA 컴포넌트 테스트

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,7 @@ import { RecoilRoot } from 'recoil';
 import { Global, ThemeProvider } from '@emotion/react';
 import globalStyle from './styles/globalStyle';
 import theme from './styles/theme';
-
-import Feedback from '@pages/Feedback/Feedback';
+import Feedback from '@pages/Feedback';
 
 function App() {
 	return (
@@ -12,7 +11,7 @@ function App() {
 			<ThemeProvider theme={theme}>
 				<div className="App">
 					<Global styles={globalStyle} />
-					<Feedback></Feedback>
+					<Feedback />
 				</div>
 			</ThemeProvider>
 		</RecoilRoot>
---
+++ frontend/src/pages/Feedback.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef } from 'react';
+import { Feedbackbox } from '@components/Feedbackbox/Feedbackbox';
+import FeedbackArea from '@components/FeedbackArea/FeedbackArea';
+
+const Feedback = () => {
+	const feedbackRef = useRef([]);
+	const dummyFeedback = [
+		{ id: 0, content: '테스트 피드백1', startTime: 3, endTime: 4 },
+		{ id: 1, content: '테스트 피드백2', startTime: 6, endTime: 9 },
+		{ id: 2, content: '테스트 피드백3', startTime: 10, endTime: 15 },
+		{ id: 3, content: '테스트 피드백4', startTime: 16, endTime: 20 },
+		{ id: 4, content: '테스트 피드백5', startTime: 23, endTime: 30 },
+		{ id: 5, content: '테스트 피드백6', startTime: 31, endTime: 33 },
+		{ id: 6, content: '테스트 피드백7', startTime: 34, endTime: 40 },
+		{ id: 7, content: '테스트 피드백8', startTime: 45, endTime: 50 },
+		{ id: 8, content: '테스트 피드백9', startTime: 51, endTime: 53 },
+		{ id: 9, content: '테스트 피드백10', startTime: 56, endTime: 57 },
+	];
+
+	useEffect(() => {
+		console.log(feedbackRef);
+	}, []);
+
+	const onClickFeedback = (e) => {
+		e.target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+	};
+
+	return (
+		<FeedbackArea>
+			<FeedbackArea.FBAScrollView>
+				{dummyFeedback.map((feedback, idx) => (
+					<Feedbackbox
+						key={feedback.id}
+						onClickFeedback={onClickFeedback}
+						ref={(elem) => (feedbackRef.current[idx] = elem)}
+					>
+						<Feedbackbox.StartTime>{feedback.startTime}</Feedbackbox.StartTime>
+						<Feedbackbox.Content>{feedback.content}</Feedbackbox.Content>
+					</Feedbackbox>
+				))}
+			</FeedbackArea.FBAScrollView>
+			<FeedbackArea.FBATextArea></FeedbackArea.FBATextArea>
+		</FeedbackArea>
+	);
+};
+
+export default Feedback;
