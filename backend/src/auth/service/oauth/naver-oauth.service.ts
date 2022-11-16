88c42730afe7358464806e865ac2c7641902eeee
AnJeongMin <ajhappy12@gmail.com>
2022-11-16 16:40:08 +0900

    feat: BE - naver oauth 페이지 url 반환 함수 구현

--- backend/src/auth/service/oauth/naver-oauth.service.ts
+++ backend/src/auth/service/oauth/naver-oauth.service.ts
@@ -1,15 +1,25 @@
+import { NAVER_ACCESS_TOKEN_URL, NAVER_AUTHORIZE_PAGE_URL, OAUTH_TYPE } from '@constant';
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 import { UserSocialInfo } from 'src/types/auth.type';
 import { OauthService } from './interface-oauth.service';
 
 @Injectable()
 export class OauthNaverService implements OauthService {
+	private clientId = process.env.NAVER_CLIENT_ID;
+	private clientSecret = process.env.NAVER_CLIENT_SECRET;
+	private callbackUrl = [
+		process.env.SERVER_URL,
+		process.env.NAVER_CALLBACK_URL,
+		OAUTH_TYPE.NAVER,
+	].join('/');
+
 	getSocialUrl(): string {
-		throw new Error('Method not implemented.');
+		const queryString = `&client_id=${this.clientId}&redirect_uri=${this.callbackUrl}`;
+		return NAVER_AUTHORIZE_PAGE_URL + queryString;
 	}
-	getAccessTokenByAuthorizationCode(authorizationCode: string): Promise<string> {
-		throw new Error('Method not implemented.');
-	}
+
+	async getAccessTokenByAuthorizationCode(authorizationCode: string): Promise<string> {}
 	getSocialInfoByAccessToken(accessToken: string): Promise<UserSocialInfo> {
 		throw new Error('Method not implemented.');
 	}
--- backend/src/constant/auth.constant.ts
+++ backend/src/constant/auth.constant.ts
@@ -1,6 +1,15 @@
 export const USER_REPOSITORY_INTERFACE = 'UserRepository' as const;
 
+// OAUTH
 export enum OAUTH_TYPE {
 	NAVER = 'naver',
 	GOOGLE = 'google',
 }
+export const OAUTH_CALLBACK_URL = 'api/auth/oauth/callback';
+
+// NAVER
+export const NAVER_AUTHORIZE_PAGE_URL =
+	'https://nid.naver.com/oauth2.0/authorize?response_type=code';
+export const NAVER_ACCESS_TOKEN_URL =
+	'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code';
+export const NAVER_PROFILE_API_URL = 'https://openapi.naver.com/v1/nid/me';
