585afce611eae98cc60ee031bbba6e3d6febcdb2
AnJeongMin <ajhappy12@gmail.com>
2022-11-17 15:02:08 +0900

    feat: BE - google oauth 구현체 변수 설정

--- backend/src/auth/service/oauth/google-oauth.service.ts
+++ backend/src/auth/service/oauth/google-oauth.service.ts
@@ -1,9 +1,18 @@
+import { OAUTH_CALLBACK_URL, OAUTH_TYPE } from '@constant';
 import { Injectable } from '@nestjs/common';
 import { UserSocialInfo } from 'src/types/auth.type';
 import { OauthService } from './interface-oauth.service';
 
 @Injectable()
 export class OauthGoogleService implements OauthService {
+	private clientId = process.env.GOGGLE_CLIENT_ID;
+	private clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+	private callbackUrl = [
+		process.env.SERVER_ORIGIN_URL,
+		OAUTH_CALLBACK_URL,
+		OAUTH_TYPE.GOOGLE,
+	].join('/');
+
 	getSocialUrl(): string {
 		throw new Error('Method not implemented.');
 	}
