fc41934e36f4b0dde728fd1452fda982210c8793
punkryn <gns816715@gmail.com>
2022-11-17 20:40:21 +0900

    test: access token, refresh token 발급 테스트

--- backend/src/auth/service/auth.service.spec.ts
+++ backend/src/auth/service/auth.service.spec.ts
@@ -1,4 +1,6 @@
-import { USER_REPOSITORY_INTERFACE } from '@constant';
+import { JWT_VALUE, USER_REPOSITORY_INTERFACE } from '@constant';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserInfo } from 'src/types/auth.type';
 import { MockRepository } from 'src/types/mock.type';
@@ -14,11 +16,17 @@ const mockUserRepository = () => ({
 	findAllUser: jest.fn(),
 });
 
+const mockJwtService = () => ({
+	sign: jest.fn(() => 'TOKEN'),
+});
+
 describe('AuthService', () => {
 	let authService: AuthService;
 	let userRepository: MockRepository<UserRepository<UserEntity>>;
 	let oauthGoogleService: OauthKakaoService;
 	let oauthNaverService: OauthNaverService;
+	let jwtService: JwtService;
+	let configService: ConfigService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -30,6 +38,11 @@ describe('AuthService', () => {
 				},
 				OauthKakaoService,
 				OauthNaverService,
+				ConfigService,
+				{
+					provide: JwtService,
+					useValue: mockJwtService(),
+				},
 			],
 		}).compile();
 
@@ -37,6 +50,8 @@ describe('AuthService', () => {
 		userRepository = module.get(USER_REPOSITORY_INTERFACE);
 		oauthNaverService = module.get(OauthNaverService);
 		oauthGoogleService = module.get(OauthKakaoService);
+		jwtService = module.get(JwtService);
+		configService = module.get(ConfigService);
 	});
 
 	describe('valid case', () => {
@@ -45,6 +60,7 @@ describe('AuthService', () => {
 			expect(userRepository).toBeDefined();
 			expect(oauthNaverService).toBeDefined();
 			expect(oauthGoogleService).toBeDefined();
+			expect(jwtService).toBeDefined();
 		});
 
 		it('유저의 OAuth로 시작 테스트', async () => {
@@ -82,6 +98,42 @@ describe('AuthService', () => {
 				expect(err).toBeInstanceOf(Error);
 				expect(err.message).toBe('');
 			}
+		});
+	});
+
+	describe('token 발급 테스트', () => {
+		it('access token 발급 테스트', () => {
+			const payload = { nickname: 'test', email: 'test@test.com' };
+
+			const token = authService.createJwt({
+				payload,
+				secret: JWT_VALUE.JWT_ACCESS_TOKEN_SECRET,
+				expirationTime: JWT_VALUE.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
+			});
+
+			expect(jwtService.sign).toHaveBeenCalledTimes(1);
+			expect(jwtService.sign).toHaveBeenCalledWith(payload, {
+				secret: process.env[JWT_VALUE.JWT_ACCESS_TOKEN_SECRET],
+				expiresIn: `${process.env[JWT_VALUE.JWT_ACCESS_TOKEN_EXPIRATION_TIME]}s`,
+			});
+			expect(token).toBe('TOKEN');
+		});
+
+		it('refresh token 발급 테스트', () => {
+			const payload = { nickname: 'test', email: 'test@test.com' };
+
+			const token = authService.createJwt({
+				payload,
+				secret: JWT_VALUE.JWT_REFRESH_TOKEN_SECRET,
+				expirationTime: JWT_VALUE.JWT_REFRESH_TOKEN_EXPIRATION_TIME,
+			});
+
+			expect(jwtService.sign).toHaveBeenCalledTimes(1);
+			expect(jwtService.sign).toHaveBeenCalledWith(payload, {
+				secret: process.env[JWT_VALUE.JWT_REFRESH_TOKEN_SECRET],
+				expiresIn: `${process.env[JWT_VALUE.JWT_REFRESH_TOKEN_EXPIRATION_TIME]}s`,
+			});
+			expect(token).toBe('TOKEN');
 		});
 	});
 
