4ebe5cfc561732f9775cb43bba43cc5df4822648
punkryn <gns816715@gmail.com>
2022-11-19 00:47:13 +0900

    refactor: testing을 위한 환경변수 추가

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 
 export const envConfig: ConfigModuleOptions = {
 	isGlobal: true, // 환경 변수를 전역으로 사용
-	envFilePath: '.env',
+	envFilePath: process.env.NODE_ENV === 'dev' ? '.env' : '.env.test',
 	// 루트 경로에서 .env 사용 (cross-env로 환경에 따른 .env 적용도 가능)
 	validationSchema: Joi.object({
 		DB_USER: Joi.string().required(),
