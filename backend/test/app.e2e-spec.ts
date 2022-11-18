c71e0b7b885ed2a2d47cb27cd2c29dd4ba4a7c17
punkryn <gns816715@gmail.com>
2022-11-18 10:58:07 +0900

    test: e2e 작성중..

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -2,20 +2,47 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { AuthModule } from './../src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { envConfig, typeormConfig } from '@config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './../src/user/user.module';
+import { AuthService } from './../src/auth/service/auth.service';
+import { NestFactory } from '@nestjs/core';
 
 describe('AppController (e2e)', () => {
 	let app: INestApplication;
+	const authService = { getSocialUrl: () => 'test' };
 
 	beforeEach(async () => {
 		const moduleFixture: TestingModule = await Test.createTestingModule({
-			imports: [AppModule],
+			imports: [
+				AppModule,
+				// AuthModule,
+				// ConfigModule.forRoot(envConfig),
+				// TypeOrmModule.forRootAsync(typeormConfig),
+				// UserModule,
+			],
 		}).compile();
 
 		app = moduleFixture.createNestApplication();
 		await app.init();
 	});
 
-	it('/ (GET)', () => {
-		return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+	it('/api/auth/oauth/redirect/:type (GET)', () => {
+		return request(app.getHttpServer()).get('/api/auth/oauth/redirect/naver').expect(301);
+	});
+	it('/api/auth/oauth/callback/:type (GET)', async () => {
+		return await request(app.getHttpServer())
+			.get('/api/auth/oauth/callback/naver?code=test')
+			.expect(200);
+	});
+
+	it('/api/auth/login (GET)', () => {
+		return request(app.getHttpServer()).get('/api/auth/login').expect(200);
+	});
+
+	afterAll(async () => {
+		await app.close();
 	});
 });
