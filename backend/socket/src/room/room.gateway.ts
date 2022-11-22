9a6056937944a2bffe963d5ed13b03efc274be44
punkryn <gns816715@gmail.com>
2022-11-22 16:30:22 +0900

    ✨ feat: (BE) - create room event 구현

--- backend/socket/src/room/room.gateway.ts
+++ backend/socket/src/room/room.gateway.ts
@@ -1,9 +1,30 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+	ConnectedSocket,
+	OnGatewayDisconnect,
+	SubscribeMessage,
+	WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { v4 as uuidv4 } from 'uuid';
 
 @WebSocketGateway()
-export class RoomGateway {
-	@SubscribeMessage('message')
-	handleMessage(client: any, payload: any): string {
+export class RoomGateway implements OnGatewayDisconnect {
+	@SubscribeMessage('create_room')
+	handleCreateRoom(): string {
+		return JSON.stringify({
+			success: true,
+			data: {
+				uuid: uuidv4(),
+			},
+		});
+	}
+
+	@SubscribeMessage('enter_room')
+	handleEnterRoom(@ConnectedSocket() client: Socket): string {
 		return 'Hello world!';
+	}
+
+	handleDisconnect(@ConnectedSocket() client: Socket) {
+		console.log('disconnected');
 	}
 }
