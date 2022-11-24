ad7cfbe2b914a493a4cfcd9c193c09ff47a5c3da
AnJeongMin <ajhappy12@gmail.com>
2022-11-24 23:56:52 +0900

    🛠️ refactor: (FE) - login 페이지 마크업 수정
    oauth button svg 적용 및 픽셀 단위를 px로 수정

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -6,7 +6,9 @@ import { authState } from '@store/auth.atom';
 import useSafeNavigate from '@hooks/useSafeNavigate';
 
 import { ReactComponent as InterfacePreview } from '@assets/icon/interface_preview.svg';
-import { ReactComponent as InterfaceLogo } from '@assets/icon/interface_logo.svg';
+import { ReactComponent as InterfaceDescript } from '@assets/icon/interface_descript.svg';
+import { ReactComponent as NaverOauthBtn } from '@assets/icon/naver_oauth.svg';
+import { ReactComponent as KakaoOauthBtn } from '@assets/icon/kakao_oauth.svg';
 import { css } from '@emotion/react';
 import axios from 'axios';
 import { useEffect } from 'react';
@@ -48,20 +50,19 @@ const Login = () => {
 
 	return (
 		<>
-			<div css={loginContainer}>
-				<InterfacePreview css={{ width: '40vw' }} />
-				<div css={{ display: 'flex', flexDirection: 'column', gap: '6vh' }}>
-					<div css={{ display: 'flex', flexDirection: 'column', gap: '1vh' }}>
-						<InterfaceLogo css={{ width: '20vw', filter: 'invert(100%)' }} />
-						<div css={{ fontSize: '1.2rem' }}> 실시간 면접 피드백 플랫폼 </div>
-					</div>
-					<div css={{ display: 'flex', flexDirection: 'column', gap: '2vh' }}>
-						<button onClick={() => startOauth(OAUTH_TYPE.NAVER)} css={OauthButton}>
-							<div> Naver </div>
-						</button>
-						<button onClick={() => startOauth(OAUTH_TYPE.KAKAO)} css={OauthButton}>
-							<div> Kakao </div>
-						</button>
+			<div css={LoginContainer}>
+				<InterfacePreview css={{ width: '800px', height: '440px' }} />
+				<div css={{ display: 'flex', flexDirection: 'column', gap: '24px' }}>
+					<InterfaceDescript css={{ width: '500px', height: '160px' }} />
+					<div css={{ display: 'flex', flexDirection: 'column' }}>
+						<NaverOauthBtn
+							css={OauthButton}
+							onClick={() => startOauth(OAUTH_TYPE.NAVER)}
+						/>
+						<KakaoOauthBtn
+							css={OauthButton}
+							onClick={() => startOauth(OAUTH_TYPE.NAVER)}
+						/>
 					</div>
 				</div>
 			</div>
@@ -69,23 +70,24 @@ const Login = () => {
 	);
 };
 
-const loginContainer = css`
+const LoginContainer = css`
 	width: 100vw;
 	height: 100vh;
 	display: flex;
 	align-items: center;
 	justify-content: center;
 	background: #030d45;
-	gap: 6vw;
+	gap: 64px;
 	color: white;
 `;
 
 const OauthButton = css`
-	width: 15vw;
-	height: 5vh;
-	border-radius: 10px;
-	background: white;
-	font-size: 1.2em;
+	width: 300px;
+	height: 80px;
+	margin-left: 32px;
+	&:hover {
+		cursor: pointer;
+	}
 `;
 
 export default Login;
