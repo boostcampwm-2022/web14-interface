d111e29bfadad469f0d765b58ffa3f208aa00dce
AnJeongMin <ajhappy12@gmail.com>
2022-11-24 22:21:06 +0900

    ✨ feat: (FE) - login page의 oauth, login 이벤트 구현

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -8,14 +8,35 @@ import useSafeNavigate from '@hooks/useSafeNavigate';
 import { ReactComponent as InterfacePreview } from '@assets/icon/interface_preview.svg';
 import { ReactComponent as InterfaceLogo } from '@assets/icon/interface_logo.svg';
 import { css } from '@emotion/react';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+enum OAUTH_TYPE {
+	NAVER = 'naver',
+	KAKAO = 'kakao',
+}
 
 const Login = () => {
 	const { safeNavigate } = useSafeNavigate();
 	const authSetter = useSetRecoilState(authState);
 
-	const handleLogin = () => {
-		safeNavigate(PHASE_TYPE.LANDING_PHASE);
-		authSetter(true);
+	useEffect(() => {
+		(async () => {
+			const res = await axios
+				.get('/api/auth/login')
+				.then((res) => res.data.json())
+				.catch((err) => console.error(err));
+
+			// TODO server에서 응답, 오류 구조화
+			if (res.statusCode === 200) {
+				safeNavigate(PHASE_TYPE.LANDING_PHASE);
+				authSetter(true);
+			}
+		})();
+	}, []);
+
+	const startOauth = async (type: string) => {
+		await axios.get(`/api/auth/oauth/redirect/${type}`);
 	};
 
 	return (
@@ -28,10 +49,10 @@ const Login = () => {
 						<div css={{ fontSize: '1.2rem' }}> 실시간 면접 피드백 플랫폼 </div>
 					</div>
 					<div css={{ display: 'flex', flexDirection: 'column', gap: '2vh' }}>
-						<button onClick={handleLogin} css={OauthButton}>
+						<button onClick={() => startOauth(OAUTH_TYPE.NAVER)} css={OauthButton}>
 							<div> Naver </div>
 						</button>
-						<button onClick={handleLogin} css={OauthButton}>
+						<button onClick={() => startOauth(OAUTH_TYPE.KAKAO)} css={OauthButton}>
 							<div> Kakao </div>
 						</button>
 					</div>
