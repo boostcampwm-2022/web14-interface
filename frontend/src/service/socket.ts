757c78a8b91d60847c60dc13f6d78184069c0970
seyoung8239 <seyoung8239@gmail.com>
2022-12-01 00:52:08 +0900

    🐋 chore: (FE) - devserver proxy 세팅

--- frontend/src/service/socket.ts
+++ frontend/src/service/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-export const socket = io(`ws://localhost:3000/`, {
+export const socket = io(process.env.REACT_APP_SOCKET_URL, {
 	transports: ['websocket'],
 });
---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+	app.use(
+		'/api',
+		createProxyMiddleware({
+			target: 'http://localhost:8080',
+			changeOrigin: true,
+		})
+	);
+};
