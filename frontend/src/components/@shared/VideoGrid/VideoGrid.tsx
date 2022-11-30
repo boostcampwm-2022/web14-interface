53b1e1d4d6a0a9e406aa446bcc2f85eaba14a79d
Janghun Lee <bhwk2980@gmail.com>
2022-12-01 04:54:22 +0900

    ✨ feat: (FE) - 마지막에 나오는 빈 줄을 표시하지 않도록 수정

--- frontend/src/components/@shared/VideoGrid/VideoGrid.tsx
+++ frontend/src/components/@shared/VideoGrid/VideoGrid.tsx
@@ -6,15 +6,17 @@ interface propType {
 }
 
 const VideoGrid = ({ children }: propType) => {
-	const repeatNumber = Array.isArray(children) ? Math.ceil(children.length ** 0.5) : 1;
+	const colNumber = Array.isArray(children) ? Math.ceil(children.length ** 0.5) : 1;
+	const rowNumber =
+		Array.isArray(children) && children.length === colNumber ** 2 ? colNumber : colNumber - 1;
 
-	return <div css={videoGridStyle(repeatNumber)}>{children}</div>;
+	return <div css={videoGridStyle(rowNumber, colNumber)}>{children}</div>;
 };
 
-const videoGridStyle = (repeatNumber: number) => css`
+const videoGridStyle = (rowNumber: number, colNumber: number) => css`
 	display: grid;
-	grid-template-rows: repeat(${repeatNumber}, 1fr);
-	grid-template-columns: repeat(${repeatNumber}, 1fr);
+	grid-template-rows: repeat(${rowNumber}, 1fr);
+	grid-template-columns: repeat(${colNumber}, 1fr);
 	gap: 2px;
 
 	video {
