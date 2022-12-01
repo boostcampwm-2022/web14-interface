a2c2ffedcd40d74bb3227aed3d4855d4c9b4811b
AnJeongMin <ajhappy12@gmail.com>
2022-12-01 23:49:03 +0900

    ✨ feat: (BE) - socket interceptor에 request runtime log 추가

--- backend/socket/src/interceptor/socket-response.interceptor.ts
+++ backend/socket/src/interceptor/socket-response.interceptor.ts
@@ -1,20 +1,22 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { filter, map, tap } from 'rxjs/operators';
+import { Socket } from 'socket.io';
 import { SocketResponseDto } from 'src/room/dto/socket-response.dto';
-import { Request } from 'express';
 
 @Injectable()
 export class SocketResponseInterceptor implements NestInterceptor {
 	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
 		const ctx = context.switchToHttp();
-		const req = ctx.getRequest<Request>();
-		console.log(req.ip);
+		const client = ctx.getRequest<Socket>();
 
+		const start = Date.now();
 		return next.handle().pipe(
+			tap(() => {
+				const end = Date.now();
+				Logger.log(`[Client ID: ${client.id}] [${end - start}ms]`);
+			}),
 			map((response) => {
-				console.log(response);
-
 				return new SocketResponseDto(response);
 			})
 		);
