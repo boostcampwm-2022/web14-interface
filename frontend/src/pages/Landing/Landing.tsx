8a3c59640d1b917787c9aae78b59478a2c132479
Janghun Lee <bhwk2980@gmail.com>
2022-12-03 05:28:04 +0900

    🐋 chore: (FE) - Landing 페이지 1차 마크업

--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -8,6 +8,27 @@ import { useSetRecoilState } from 'recoil';
 import { SOCKET_EVENT_TYPE } from '@constants/event.constant';
 import { socketEmit } from '../../api/socket.api';
 import { UserDTO } from '@customType/user';
+import { ReactComponent as InterfacePreview } from '@assets/preview.svg';
+import { ReactComponent as Logo } from '@assets/logo_black.svg';
+import { ReactComponent as FolderIcon } from '@assets/icon/folder.svg';
+import { ReactComponent as PlusIcon } from '@assets/icon/plus.svg';
+import {
+	folderIconStyle,
+	headerBtnGroupStyle,
+	headerStyle,
+	historyBtn,
+	landingWrapperStyle,
+	LargeBtn,
+	logoStyle,
+	logoutBtn,
+	mainBtnGroupStyle,
+	mainStyle,
+	plusIconStyle,
+	previewStyle,
+	primaryBtn,
+	secondaryBtn,
+	MediumBtn,
+} from './Landing.style';
 
 interface createRoomResponseType {
 	uuid: string;
@@ -22,29 +43,26 @@ const Landing = () => {
 	usePreventLeave();
 	const [roomNumber, setRoomNumber] = useState('');
 	const setRoom = useSetRecoilState(roomUUIDState);
-	const setOthers = useSetRecoilState(othersInRoomState);
-	const setMe = useSetRecoilState(meInRoomState);
+	const setOthers = useSetRecoilState<UserDTO[]>(othersInRoomState);
+	const setMe = useSetRecoilState<UserDTO>(meInRoomState);
 
 	const { safeNavigate } = useSafeNavigate();
 
 	const handleSignOut = async () => {
 		const res = await axios.get('/api/auth/logout');
 		alert(res.data.statusCode);
-	};
-
-	const handleAttendRoom = () => {
-		attendRoom(roomNumber);
+		safeNavigate(PHASE_TYPE.LOGIN_PHASE);
 	};
 
 	const handleCreate = async () => {
 		const { uuid } = await socketEmit<createRoomResponseType>(SOCKET_EVENT_TYPE.CRERATE_ROOM);
 		setRoom(uuid);
-		attendRoom(uuid);
+		handleAttendRoom(uuid);
 	};
 
-	const attendRoom = async (roomUUID) => {
+	const handleAttendRoom = async (roomUUID) => {
 		const { others, me } = await socketEmit<attendRoomResponseType>(
-			SOCKET_EVENT_TYPE.COUNT_FEEDBACK,
+			SOCKET_EVENT_TYPE.ENTER_ROOM,
 			roomUUID
 		);
 
@@ -53,22 +71,45 @@ const Landing = () => {
 		safeNavigate(PHASE_TYPE.LOBBY_PHASE);
 	};
 
+	//TODO 닉네임 로딩 반영 -> 소켓 Suspense howt?
 	return (
-		<>
-			<div>Landing</div>
-			<input
-				type="text"
-				placeholder="방번호를 입력하세요"
-				value={roomNumber}
-				onChange={(e) => setRoomNumber(e.target.value)}
-			/>
-			<button onClick={handleCreate}>방생성</button>
-
-			<button onClick={handleAttendRoom}>방참가</button>
-			<button onClick={handleSignOut}>로그아웃</button>
-			<button onClick={handleSignOut}>내 기록</button>
-			<button onClick={handleSignOut}>로그아웃</button>
-		</>
+		<div css={landingWrapperStyle}>
+			<header css={headerStyle}>
+				<button>
+					<Logo css={logoStyle} />
+				</button>
+				<div css={headerBtnGroupStyle}>
+					<button css={[MediumBtn, historyBtn]}>
+						<FolderIcon css={folderIconStyle} />
+						기록
+					</button>
+					<button css={[MediumBtn, logoutBtn]} onClick={handleSignOut}>
+						로그아웃
+					</button>
+				</div>
+			</header>
+			<main css={mainStyle}>
+				<span>interface님, 안녕하세요!</span>
+				<div css={mainBtnGroupStyle}>
+					<button css={[LargeBtn, primaryBtn]} onClick={handleCreate}>
+						<PlusIcon css={plusIconStyle} />방 만들기
+					</button>
+					<button
+						css={[LargeBtn, secondaryBtn]}
+						onClick={() => handleAttendRoom(roomNumber)}
+					>
+						참가하기
+					</button>
+					<input
+						type="text"
+						placeholder="방번호를 입력하세요"
+						value={roomNumber}
+						onChange={(e) => setRoomNumber(e.target.value)}
+					/>
+				</div>
+				<InterfacePreview css={previewStyle} />
+			</main>
+		</div>
 	);
 };
 
