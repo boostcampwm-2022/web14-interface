16bdfdc5ed7034856e09e6ec3ae78c95f51f4fdb
AnJeongMin <ajhappy12@gmail.com>
2022-12-02 16:05:18 +0900

    ✨ feat: (BE) - socket server에 webRTC connection 구현

--- backend/socket/src/room/room.gateway.ts
+++ backend/socket/src/room/room.gateway.ts
@@ -12,8 +12,10 @@ import {
 import { Namespace, Socket } from 'socket.io';
 import { SocketExceptionFilter } from 'src/filter/socket-exception.filter';
 import { SocketResponseInterceptor } from 'src/interceptor/socket-response.interceptor';
+import { WebrtcAnswerDto, WebrtcIcecandidateDto, WebrtcOfferDto } from './dto/webrtc.dto';
 import { ConnectionService } from './service/connection/connection.service';
 import { InterviewService } from './service/interview/interview.service';
+import { WebrtcService } from './service/webRTC/webrtc.service';
 
 @UseInterceptors(new SocketResponseInterceptor())
 @UseFilters(new SocketExceptionFilter())
@@ -24,7 +26,8 @@ export class RoomGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
 	constructor(
 		private readonly connectionService: ConnectionService,
-		private readonly interviewService: InterviewService
+		private readonly interviewService: InterviewService,
+		private readonly webrtcService: WebrtcService
 	) {}
 
 	// connection
@@ -51,6 +54,7 @@ export class RoomGateway implements OnGatewayConnection, OnGatewayDisconnect {
 	handleDisconnect(@ConnectedSocket() client: Socket) {
 		this.logger.log(`disconnected: ${client.id}`);
 		this.connectionService.leaveRoom({ client, server: this.server });
+		this.webrtcService.disconnectWebrtc({ client, server: this.server });
 	}
 
 	// interview
@@ -68,5 +72,42 @@ export class RoomGateway implements OnGatewayConnection, OnGatewayDisconnect {
 	@SubscribeMessage(EVENT.END_FEEDBACK)
 	handleEndFeedback(@ConnectedSocket() client: Socket) {
 		return this.interviewService.endFeedback({ client, server: this.server });
+	}
+
+	// webRTC
+
+	@SubscribeMessage(EVENT.START_SIGNALING)
+	handleStartSignaling(@ConnectedSocket() client: Socket) {
+		return this.webrtcService.startSignaling({ client, server: this.server });
+	}
+
+	@SubscribeMessage(EVENT.OFFER)
+	handleOffer(@ConnectedSocket() client: Socket, @MessageBody() connectSignal: WebrtcOfferDto) {
+		return this.webrtcService.delivery({
+			server: this.server,
+			connectSignal,
+			eventType: EVENT.OFFER,
+		});
+	}
+
+	@SubscribeMessage(EVENT.ANSWER)
+	handleAnswer(@ConnectedSocket() client: Socket, @MessageBody() connectSignal: WebrtcAnswerDto) {
+		return this.webrtcService.delivery({
+			server: this.server,
+			connectSignal,
+			eventType: EVENT.ANSWER,
+		});
+	}
+
+	@SubscribeMessage(EVENT.ICECANDIDATE)
+	handleIceCandidate(
+		@ConnectedSocket() client: Socket,
+		@MessageBody() connectSignal: WebrtcIcecandidateDto
+	) {
+		return this.webrtcService.delivery({
+			server: this.server,
+			connectSignal,
+			eventType: EVENT.ICECANDIDATE,
+		});
 	}
 }
