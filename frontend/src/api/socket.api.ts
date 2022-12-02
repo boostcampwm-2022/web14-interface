07756a5cf09d3fb8a71a04871c628dd6580acb4f
Janghun Lee <bhwk2980@gmail.com>
2022-12-03 04:48:16 +0900

    🐋 chore: (FE) - socketEmit에 디버깅용 주석 추가

--- frontend/src/api/socket.api.ts
+++ frontend/src/api/socket.api.ts
@@ -9,6 +9,7 @@ interface socketResponseType<T> {
 export const socketEmit = <T>(event, data?: T) => {
 	return new Promise<T>((resolve) => {
 		socket.emit(event, data, ({ success, data, message }: socketResponseType<T>) => {
+			console.log('socket.api', success, data, message);
 			if (!success) {
 				//TODO 여기서 에러 처리
 				//모달을 띄우던 -> useModal
