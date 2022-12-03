4c2a15d3ac613bf0ac074f2a22007d63dbc19cc1
seyoung8239 <seyoung8239@gmail.com>
2022-12-03 22:23:12 +0900

    🐞 fix: (FE) - safeNavigator 타이밍 문제

--- frontend/src/hooks/useSafeNavigate.ts
+++ frontend/src/hooks/useSafeNavigate.ts
@@ -1,24 +1,16 @@
 import { useNavigate } from 'react-router-dom';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 import { phaseState } from '@store/phase.atom';
 import { PHASE_TYPE } from '@constants/phase.constant';
 import { getPathWithPhase } from '@utils/getPathWithPhase';
-import { useCallback } from 'react';
 
 const useSafeNavigate = () => {
 	const navigate = useNavigate();
-	const [phase, phaseSetter] = useRecoilState(phaseState);
+	const setPhase = useSetRecoilState(phaseState);
 	const safeNavigate = (newPhase: PHASE_TYPE) => {
-		phaseSetter(newPhase);
-		setTimeout(() => navigateTo(newPhase), 100);
+		setPhase(newPhase);
+		setTimeout(() => navigate(getPathWithPhase(newPhase)));
 	};
-	//TODO: 상태 타이밍 문제
-	const navigateTo = useCallback(
-		(phase) => {
-			navigate(getPathWithPhase(phase));
-		},
-		[phase]
-	);
 	return { safeNavigate };
 };
 export default useSafeNavigate;
