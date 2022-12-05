5c091e4c90c0e289825d55528a6ca52a09b563b6
punkryn <gns816715@gmail.com>
2022-12-05 19:35:07 +0900

    ✨ feat: (BE) - 방 인원이 없을 때 방 삭제

--- backend/socket/src/room/service/connection/connection.service.ts
+++ backend/socket/src/room/service/connection/connection.service.ts
@@ -91,6 +91,11 @@ export class ConnectionService {
 
 		client.leave(roomUUID);
 
+		const usersInRoom = this.roomRepository.getUsersInRoom(roomUUID);
+		if (!usersInRoom.length) {
+			this.roomRepository.deleteRoom(roomUUID);
+		}
+
 		return {};
 	}
 
