37fc82aa49e27b36f2545cbc03fdde19127b1e34
AnJeongMin <ajhappy12@gmail.com>
2022-12-07 17:56:03 +0900

    ✨ feat: (BE) - rest server exception filter 구현
    Co-authored-by: punkryn <gns816715@gmail.com>

---
+++ backend/rest/src/filter/http-exception.filter.ts
@@ -0,0 +1,40 @@
+import {
+	ArgumentsHost,
+	Catch,
+	ExceptionFilter,
+	HttpException,
+	InternalServerErrorException,
+	Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+	catch(exception: Error, host: ArgumentsHost) {
+		const logger = new Logger('EXCEPTION');
+
+		const ctx = host.switchToHttp();
+		const res = ctx.getResponse<Response>();
+		const req = ctx.getRequest<Request>();
+
+		if (!(exception instanceof HttpException)) {
+			exception = new InternalServerErrorException();
+		}
+
+		const { statusCode, message, error }: any = (exception as HttpException).getResponse();
+
+		logger.error(`[Request URL] ${req.url}`);
+		logger.error(`[Exception Time] ${new Date().toISOString()}`);
+		logger.error(`[Exception Name] ${error}`);
+		logger.error(`[Exception Message] ${message}`);
+		logger.error(`[Exception Stack] ${exception.stack}`);
+
+		const response = {
+			name: error,
+			message: message,
+			stack: exception.stack,
+		};
+
+		res.status(statusCode).json(response);
+	}
+}
