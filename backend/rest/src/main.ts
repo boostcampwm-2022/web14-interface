0b37b3dce16779c111ddc893389a115eb8664c14
AnJeongMin <ajhappy12@gmail.com>
2022-12-07 17:46:29 +0900

    ✨ feat: (BE) - rest server interceptor 구현
    Co-authored-by: punkryn <gns816715@gmail.com>

--- backend/rest/src/main.ts
+++ backend/rest/src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { setupSwagger } from '@config';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
+import { RestInterceptor } from './interceptor/rest.interceptor';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix('api');
 	app.use(helmet());
 	app.use(cookieParser());
+	app.useGlobalInterceptors(new RestInterceptor());
 	setupSwagger(app);
 
 	await app.listen(8080);
