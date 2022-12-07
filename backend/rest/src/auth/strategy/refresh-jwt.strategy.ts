7ba2b62c59abe8cfef6b67488eea4d2434f56beb
AnJeongMin <ajhappy12@gmail.com>
2022-12-07 18:48:28 +0900

    🛠️ refactor: (BE) - jwt 재발급 로직 변경
    Co-authored-by: punkryn <gns816715@gmail.com>

--- backend/rest/src/auth/strategy/refresh-jwt.strategy.ts
+++ backend/rest/src/auth/strategy/refresh-jwt.strategy.ts
@@ -1,12 +1,11 @@
-import { accessTokenOptions, JWT_ENV } from '@constant';
+import { JWT_ENV } from '@constant';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Request } from 'express';
 import { AuthService } from '../service/auth.service';
-import { JwtPayload } from 'src/types/auth.type';
-import { JwtPayloadBuiler } from 'src/auth/dto/create-jwt.builder';
+import { JwtPayload, UserInfo } from 'src/types/auth.type';
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
@@ -17,7 +16,7 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh'
 		super({
 			ignoreExpiration: false,
 			jwtFromRequest: ExtractJwt.fromExtractors([
-				(req) => {
+				(req: Request) => {
 					const token = req?.cookies.refreshToken;
 					return token ?? null;
 				},
@@ -29,13 +28,13 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh'
 
 	async validate(req: Request, payload: JwtPayload): Promise<JwtPayload> {
 		const { id, email } = payload;
-		const createJwtPayload = new JwtPayloadBuiler().setId(id).setEmail(email).build();
-		const accessToken = this.authService.createJwt({
-			payload: createJwtPayload,
-			...accessTokenOptions,
-		});
+		const { accessToken, refreshToken } = this.authService.createAccessTokenAndRefreshToken({
+			id,
+			email,
+		} as UserInfo);
 
 		req.cookies.accessToken = accessToken;
+		req.cookies.refreshToken = refreshToken;
 
 		return payload;
 	}
