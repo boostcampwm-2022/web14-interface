ca23bede71197242faf65c2a3d4aa18a2bc22b43
seyoung8239 <seyoung8239@gmail.com>
2022-12-08 18:53:40 +0900

    📖 docs: (FE) - FeedbackItem컴포넌트 storybook 추가

--- frontend/src/components/FeedbackItem/FeedbackItem.tsx
+++ frontend/src/components/FeedbackItem/FeedbackItem.tsx
@@ -8,15 +8,17 @@ import { currentVideoTimeState } from '@store/currentVideoTime.store';
 import { feedbackBoxStyle, fbTextAreaStyle, fbStartTimeStyle } from './FeedbackItem.style';
 
 import { FeedbackItemType } from '@customType/feedback';
+import { mmssFormatter } from '@utils/common.util';
+import { ONE_SECOND } from '@constants/time.constant';
 
-interface PropsType {
+export interface Props {
 	feedback: FeedbackItemType;
 	//TODO ref type any
 	feedbackRef: React.MutableRefObject<any[]>;
 	index: number;
 	editableBtns: React.ReactNode;
 }
-const FeedbackItem = ({ feedback, feedbackRef, index, editableBtns }: PropsType) => {
+const FeedbackItem = ({ feedback, feedbackRef, index, editableBtns }: Props) => {
 	const textareaRef = useRef<HTMLTextAreaElement>(null);
 	const isFbSync = useRecoilValue(isFbSyncState);
 	const setIsFbClicked = useSetRecoilState(isFbClickedState);
@@ -24,7 +26,11 @@ const FeedbackItem = ({ feedback, feedbackRef, index, editableBtns }: PropsType)
 	const { handleFbChange } = useCrudFeedback(feedback.id);
 
 	useEffect(() => {
-		feedbackRef.current[index].style.height = textareaRef.current.scrollHeight + 'px';
+		const effect = () => {
+			if (!feedbackRef) return;
+			feedbackRef.current[index].style.height = textareaRef.current.scrollHeight + 'px';
+		};
+		effect();
 	});
 
 	const { startTime, isFirst, content, readOnly } = feedback;
@@ -35,10 +41,15 @@ const FeedbackItem = ({ feedback, feedbackRef, index, editableBtns }: PropsType)
 		setCurrentVideoTime(startTime);
 	};
 
+	const getFbRef = (el) => {
+		if (!feedbackRef) return;
+		return (feedbackRef.current[index] = el);
+	};
+
 	return (
-		<div ref={(el) => (feedbackRef.current[index] = el)} css={feedbackBoxStyle}>
+		<div ref={getFbRef} css={feedbackBoxStyle}>
 			<div css={fbStartTimeStyle} style={{ visibility: isFirst ? 'visible' : 'hidden' }}>
-				{startTime}
+				{mmssFormatter(startTime * ONE_SECOND)}
 			</div>
 			<textarea
 				ref={textareaRef}
