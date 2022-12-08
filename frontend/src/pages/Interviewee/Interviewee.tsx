003c1a235b87530b9217397c9ce29664b62a73c6
Seyoung Park <53702978+seyoung8239@users.noreply.github.com>
2022-12-09 03:08:06 +0900

    Revert "feedback 종료 이벤트가 emit되지 않는 문제"

--- frontend/src/pages/Interviewee/Interviewee.tsx
+++ frontend/src/pages/Interviewee/Interviewee.tsx
@@ -41,7 +41,7 @@ const Interviewee = () => {
 		socket.on(SOCKET_EVENT_TYPE.START_WAITING, ({ docsUUID }) => {
 			setDocsUUID(docsUUID);
 			stopStream(docsUUID);
-			const docsRequestDto: DocsReqDtoType = {
+			const docsRequestDTO: DocsReqDtoType = {
 				roomUUID: interviewee.roomUUID,
 				docsUUID,
 				videoPlayTime: currentVideoTime,
