561f5ab95761a63af992f7a6b4616fcee2fe992f
punkryn <gns816715@gmail.com>
2022-12-08 14:43:07 +0900

    🛠️ refactor: (BE) - 사용하지 않는 constant 제거

--- backend/rest/src/constant/http.constant.ts
+++ backend/rest/src/constant/http.constant.ts
@@ -2,6 +2,5 @@ export enum HTTP_ERROR_MSG {
 	UNKNOWN_OAUTH_TYPE_ERROR = '알 수 없는 Oatuh Type입니다.',
 	UNAUTHORIZATION_ERROR = 'social 인증이 되지 않았습니다.',
 	NULL_POINT_EXCEPTION = 'DB에 삭제할 대상이 없습니다.',
-	UNEXPECTED_UNDEFINED_OCCUR = '예기치 못 한 undefined 발생',
 	DOCS_NOT_FOUND = '매치되는 docs가 없습니다.',
 }
