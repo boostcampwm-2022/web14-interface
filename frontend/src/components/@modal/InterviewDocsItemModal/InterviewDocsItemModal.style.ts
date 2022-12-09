be57ccb240cd462368372f19b8d47c28e855ac69
seyoung8239 <seyoung8239@gmail.com>
2022-12-10 05:37:14 +0900

    ✨ feat: (FE) - InterviewDocsItemModal 컴포넌트 추가

---
+++ frontend/src/components/@modal/InterviewDocsItemModal/InterviewDocsItemModal.style.ts
@@ -0,0 +1,54 @@
+import { css } from '@emotion/react';
+
+export const docsItemWrapperStyle = css`
+	display: flex;
+	flex-direction: column;
+	gap: 15px;
+	padding: 10px;
+	height: 80vh;
+	width: 100%;
+`;
+
+export const docsItemHeaderStyle = css`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+`;
+
+export const docsItemBtnsStyle = css`
+	display: flex;
+`;
+
+export const docsItemBodyStyle = css`
+	display: flex;
+	gap: 15px;
+`;
+
+export const docsItemVideoAreaStyle = css`
+	display: flex;
+	flex-direction: column;
+	gap: 15px;
+`;
+
+export const docsItemVideoInfoStyle = css`
+	display: flex;
+	flex-direction: column;
+	gap: 5px;
+	div {
+		display: 'flex';
+	}
+`;
+
+export const docsItemFbAreaStyle = css`
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+`;
+
+export const docsItemFbBtnsStyle = css`
+	display: flex;
+`;
+
+export const docsItemFbListStyle = css`
+	overflow: scroll;
+`;
