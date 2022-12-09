8e3e1e9d8b48bbed2fbb7d62391bd160f63efede
seyoung8239 <seyoung8239@gmail.com>
2022-12-10 00:43:41 +0900

    🛠️ refactor: (FE) - fbList label first 로직 추출

--- frontend/src/utils/common.util.ts
+++ frontend/src/utils/common.util.ts
@@ -1,4 +1,5 @@
 import { ONE_MINUTE, ONE_SECOND } from '@constants/time.constant';
+import { EditableFeedbackType, FeedbackItemType, FeedbackType } from '@customType/feedback';
 
 export const mmssFormatter = (ms: number) => {
 	const mm = paddingFormatter(ms / ONE_MINUTE);
@@ -13,4 +14,19 @@ const paddingFormatter = (n: number) => {
 	const l = (n + '').length;
 	const sliceSize = l > 2 ? l : 2;
 	return ('00' + n).slice(-sliceSize);
+};
+
+export const getFirstLabeledFbList = (
+	fbList: EditableFeedbackType[] | FeedbackType[]
+): FeedbackItemType[] => {
+	let prev = -1;
+	return fbList.map((fb) => {
+		fb.isFirst = false;
+		if (fb.startTime > prev) {
+			prev = fb.startTime;
+			fb.isFirst = true;
+		}
+
+		return fb;
+	});
 };
