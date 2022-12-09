bd83776aa3953f71884abdbb3acbcc7c1c8b110f
seyoung8239 <seyoung8239@gmail.com>
2022-12-10 08:00:25 +0900

    📖 docs: (FE) - RoundButton storybook 추가

--- frontend/src/components/@shared/RoundButton/RoundButton.style.ts
+++ frontend/src/components/@shared/RoundButton/RoundButton.style.ts
@@ -3,14 +3,13 @@ import { css } from '@emotion/react';
 export const roundButtonStyle = (style) => css`
 	display: flex;
 	box-sizing: border-box;
-	justify-content: center;
+	justify-content: space-around;
 	align-items: center;
 
 	width: ${style.width}px;
 	height: ${style.height}px;
 	border-radius: ${style.height / 2}px;
 	background-color: ${style.backgroundColor};
-	color: ${style.color};
-	padding: 0px 30px;
+	color: ${style.color || 'white'};
 	font-size: ${style.fontsize || '24px'};
 `;
