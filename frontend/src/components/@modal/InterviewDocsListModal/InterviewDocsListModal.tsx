5ce0446213473bedaf2fe7b77720d430c3bb8071
seyoung8239 <seyoung8239@gmail.com>
2022-12-10 05:37:47 +0900

    ✨ feat: (FE) - InterviewDocsListModal 컴포넌트 추가

--- frontend/src/components/@modal/InterviewDocsListModal/InterviewDocsListModal.tsx
+++ frontend/src/components/@modal/InterviewDocsListModal/InterviewDocsListModal.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import Modal from '@components/@shared/Modal/Modal';
+import { useRecoilValue } from 'recoil';
+import { docsListQuery } from '@store/interviewDocs.store';
+import DocsItem from '@components/DocsItem/DocsItem';
+import Button from '@components/@shared/Button/Button';
+import {
+	docsListBodyStyle,
+	docsListHeaderStyle,
+	docsListWrapperStyle,
+} from './InterviewDocsListModal.style';
 
 const InterviewDocsListModal = () => {
-	return <Modal>InterviewDocsItemModal</Modal>;
+	const docsList = useRecoilValue(docsListQuery(''));
+	return (
+		<Modal>
+			<div css={docsListWrapperStyle}>
+				<div css={docsListHeaderStyle}>
+					<div>인터뷰 기록</div>
+					<Button size="small">닫기</Button>
+				</div>
+				<div css={docsListBodyStyle}>
+					{docsList.map((docs, idx) => (
+						<DocsItem key={docs.id} docs={docs} idx={idx} style="list" />
+					))}
+				</div>
+			</div>
+		</Modal>
+	);
 };
 
 export default InterviewDocsListModal;
