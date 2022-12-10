9ba85f98cbd61ab3453a2fbcc62ef362ea5305d3
AnJeongMin <ajhappy12@gmail.com>
2022-12-10 19:51:04 +0900

    🛠️ refactor: (BE) - 환경변수 추가 및 설정 변경

---
+++ backend/socket/src/constant/object-storage.constant.ts
@@ -0,0 +1,5 @@
+export const OBJECT_STORAGE_ENDPOINT = 'https://kr.object.ncloudstorage.com';
+export const AWS_S3_RESION = 'kr-standard';
+export const BUCKET_NAME = 'interface';
+export const VIDEO_MAX_COUNT = 10;
+export const BUCKET_CORS_ALLOW_SEC = 60;
