e21f9ddc50066bb4938260f8056e73b0ddb2b7fd
Janghun Lee <bhwk2980@gmail.com>
2022-12-11 23:53:30 +0900

    🛠️ refactor: (FE) - interviewDocsItem에 openModal 변경 적용

--- frontend/src/components/InterviewDocsItem/InterviewDocsItem.tsx
+++ frontend/src/components/InterviewDocsItem/InterviewDocsItem.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import { DocsItemDtoType } from '@customType/dto';
 import { ReactComponent as DownloadIcon } from '@assets/icon/download.svg';
 import { iconSmStyle } from '@styles/commonStyle';
-import { createdAtStyle, docsItemWrapper, indexStyle, playTimeStyle } from './InterviewDocsItem.style';
+import {
+	createdAtStyle,
+	docsItemWrapper,
+	indexStyle,
+	playTimeStyle,
+} from './InterviewDocsItem.style';
 import { mmssFormatter } from '@utils/common.util';
 import { ONE_SECOND } from '@constants/time.constant';
 import useModal from '@hooks/useModal';
-import { MODAL_TYPE } from '@constants/modal.constant';
 
 export interface Props {
 	docs: DocsItemDtoType;
@@ -18,7 +22,7 @@ export interface Props {
 const InterviewDocsItem = ({ docs, idx, style }: Props) => {
 	const { openModal } = useModal();
 	const handleClickDocsItem = () => {
-		openModal(MODAL_TYPE.InterviewDocsItemModal, { docsUUID: docs.id, idx });
+		openModal('InterviewDocsItemModal', { docsUUID: docs.id, idx });
 	};
 	return (
 		<article onClick={handleClickDocsItem} css={(theme) => docsItemWrapper(theme, style)}>
