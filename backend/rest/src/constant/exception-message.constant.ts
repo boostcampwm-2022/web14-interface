0677eb3d9a45171f68d4740cf5e6c19234df4643
AnJeongMin <ajhappy12@gmail.com>
2022-12-11 16:45:08 +0900

    ✨ feat: (BE) - interview docs 삭제 api 구현

---
+++ backend/rest/src/constant/exception-message.constant.ts
@@ -0,0 +1,7 @@
+export enum HTTP_ERROR_MSG {
+	UNKNOWN_OAUTH_TYPE_ERROR = '알 수 없는 OAuth Type입니다.',
+	OAUTH_AUTHENTICATION_CANCEL = 'Social 인증이 되지 않았습니다.',
+	NOT_FOUND_TARGET_IN_DATABASE = 'DataBase에 삭제할 대상이 존재하지 않습니다.',
+	NOT_FOUND_MATCHED_DOCS = 'feedback을 저장할 interview docs가 존재하지 않습니다.',
+	CANT_DELETE_ANOTHER_DOCS = '다른 유저의 interview docs를 삭제할 수 없습니다.',
+}
