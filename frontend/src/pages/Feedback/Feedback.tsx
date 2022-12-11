3ce19ff8663f183f9b2e4304ab37a2a4fc1fac8f
Janghun Lee <bhwk2980@gmail.com>
2022-12-11 23:52:00 +0900

    🛠️ refactor: (FE) - Feedback에 피드백 종료 모달 적용

--- frontend/src/pages/Feedback/Feedback.tsx
+++ frontend/src/pages/Feedback/Feedback.tsx
@@ -1,29 +1,22 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 import IntervieweeVideo from '@components/IntervieweeVideo/IntervieweeVideo';
 import FeedbackList from '@components/FeedbackList/FeedbackList';
 import BottomBar from '@components/BottomBar/BottomBar';
 import RoundButton from '@components/@shared/RoundButton/RoundButton';
 import usePreventLeave from '@hooks/usePreventLeave';
-import useSafeNavigate from '@hooks/useSafeNavigate';
 import useCleanupInterview from '@hooks/useCleanupInterview';
-import { feedbackDtoSelector, feedbackListSelector, isFbSyncState } from '@store/feedback.store';
-import { meInRoomState } from '@store/room.store';
-import { completedFbCntState, docsUUIDState } from '@store/interview.store';
+import { feedbackListSelector, isFbSyncState } from '@store/feedback.store';
 
 import { ReactComponent as LinkIcon } from '@assets/icon/link.svg';
 import { socket } from '../../service/socket';
 import { feedbackWrapperStyle, feedbackContainerStyle, feedbackAreaStyle } from './Feedback.style';
-import { PAGE_TYPE } from '@constants/page.constant';
 import theme from '@styles/theme';
 import { iconBgStyle } from '@styles/commonStyle';
-import { socketEmit } from '@api/socket.api';
 import { SOCKET_EVENT_TYPE } from '@constants/socket.constant';
-import { FeedbackDtoType } from '@customType/dto';
 import FeedbackForm from '@components/FeedbackForm/FeedbackForm';
-import { REST_TYPE } from '@constants/rest.constant';
+import useModal from '@hooks/useModal';
 
 interface endFeedbackResponseType {
 	isLastFeedback: boolean;
@@ -32,33 +25,17 @@ interface endFeedbackResponseType {
 
 const Feedback = () => {
 	usePreventLeave();
-	const { safeNavigate } = useSafeNavigate();
+	const { openModal } = useModal();
 	const cleanupInterview = useCleanupInterview();
 
-	const setCompletedFbCnt = useSetRecoilState(completedFbCntState);
 	const [isFbSync, setIsFbSync] = useRecoilState(isFbSyncState);
-	const docsUUID = useRecoilValue(docsUUIDState);
-	const feedbackListDto = useRecoilValue(feedbackDtoSelector);
 	const feedbackList = useRecoilValue(feedbackListSelector);
 
-	const me = useRecoilValue(meInRoomState);
 	const [videoUrl, setVideoUrl] = useState('');
 
-	const handleEndFeedback = useCallback(async () => {
-		const { isLastFeedback, count } = await socketEmit<endFeedbackResponseType>(
-			SOCKET_EVENT_TYPE.END_FEEDBACK
-		);
-		setCompletedFbCnt(count);
-		const feedbackDto: FeedbackDtoType = {
-			docsUUID,
-			userUUID: me.uuid,
-			feedbackList: feedbackListDto,
-		};
-		axios.post(REST_TYPE.FEEDBACK, feedbackDto);
-
-		if (isLastFeedback) safeNavigate(PAGE_TYPE.LOBBY_PAGE);
-		else safeNavigate(PAGE_TYPE.WAITTING_PAGE);
-	}, [docsUUID, feedbackListDto, me]);
+	const handleEndFeedback = () => {
+		openModal('EndFeedbackModal');
+	};
 
 	useEffect(() => {
 		socket.on(SOCKET_EVENT_TYPE.DOWNLOAD_VIDEO, ({ videoUrl }) => {
