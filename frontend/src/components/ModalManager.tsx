1064e3f05a3dc5f9c2f06e8b46c0d7bd20eeca67
Janghun Lee <bhwk2980@gmail.com>
2022-12-11 23:44:14 +0900

    🛠️ refactor: (FE) - modal.constant 삭제 및 modalStore의 key로 타입 생성

--- frontend/src/components/ModalManager.tsx
+++ frontend/src/components/ModalManager.tsx
@@ -2,21 +2,35 @@ import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { currentModalState } from '@store/currentModal.store';
 import ReactDOM from 'react-dom';
-import EnterRoomModal from './@modal/EnterRoomModal';
+import EnterRoomModal from './@modal/EnterRoomModal/EnterRoomModal';
 import InterviewDocsListModal from './@modal/InterviewDocsListModal/InterviewDocsListModal';
 import InterviewDocsItemModal from './@modal/InterviewDocsModal/InterviewDocsModal';
+import CopyTextFieldModal from './@modal/CopyTextFieldModal/CopyTextFieldModal';
+import StartInterviewModal from './@modal/StartInterviewModal/StartInterviewModal';
+import ExitRoomModal from './@modal/ExitRoomModal/StartInterviewModal/ExitRoomModal';
+import CancelInterviewModal from './@modal/CancelInterviewModal/CancelInterviewModal';
+import EndInterviewModal from './@modal/EndInterviewModal/EndInterviewModal';
+import EndFeedbackModal from './@modal/EndFeedbackModal/EndFeedbackModal';
+
+export type MODAL_TYPE = keyof typeof modalStore;
+
+//TODO 다이나믹 import로 변경
+const modalStore = {
+	EnterRoomModal,
+	InterviewDocsListModal,
+	InterviewDocsItemModal,
+	CopyTextFieldModal,
+	StartInterviewModal,
+	ExitRoomModal,
+	EndInterviewModal,
+	CancelInterviewModal,
+	EndFeedbackModal,
+};
 
 const ModalManager = () => {
 	const currentModal = useRecoilValue(currentModalState);
 
 	if (!currentModal) return;
-
-	//TODO 다이나믹 import로 변경
-	const modalStore = {
-		EnterRoomModal,
-		InterviewDocsListModal,
-		InterviewDocsItemModal,
-	};
 
 	const ModalComponent = modalStore[currentModal.modal];
 
