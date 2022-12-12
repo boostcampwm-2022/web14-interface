28bd143da5a6674d5cfbdf4e26561eae531496ed
punkryn <gns816715@gmail.com>
2022-12-12 23:29:17 +0900

    📖 docs: (BE) - swagger 보안 미들웨어 적용

--- backend/rest/src/main.ts
+++ backend/rest/src/main.ts
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import { RestInterceptor } from './interceptor/http.interceptor';
 import { HttpExceptionFilter } from './filter/http-exception.filter';
+import expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -13,6 +14,17 @@ async function bootstrap() {
 	app.use(cookieParser());
 	app.useGlobalInterceptors(new RestInterceptor());
 	app.useGlobalFilters(new HttpExceptionFilter());
+
+	app.use(
+		['/docs/api'],
+		expressBasicAuth({
+			challenge: true,
+			users: {
+				[process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+			},
+		})
+	);
+
 	setupSwagger(app);
 
 	await app.listen(8080);
