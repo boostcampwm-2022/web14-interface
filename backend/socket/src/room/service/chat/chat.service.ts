e7ae39529d6716edcda4f3c3c323e796dd9a5b4a
AnJeongMin <ajhappy12@gmail.com>
2022-12-12 23:49:09 +0900

    ✨ feat: (BE) - socket server 채팅 이벤트 핸들링 구현

---
+++ backend/socket/src/room/service/chat/chat.service.ts
@@ -0,0 +1,65 @@
+import { EVENT, EXCEPTION_MESSAGE } from '@constant';
+import { Inject, Injectable } from '@nestjs/common';
+import { WsException } from '@nestjs/websockets';
+import { User, userUUID } from '@types';
+import { Namespace, Socket } from 'socket.io';
+import { ROOM_REPOSITORY_INTERFACE } from '@constant';
+import { ChatRequestDto, ChatTarget } from 'src/room/dto/chat.dto';
+import { RoomRepository } from 'src/room/repository/room.repository';
+
+@Injectable()
+export class ChatService {
+	constructor(
+		@Inject(ROOM_REPOSITORY_INTERFACE)
+		private readonly roomRepository: RoomRepository
+	) {}
+
+	async handleChat({
+		client,
+		server,
+		chatRequestDto,
+	}: {
+		client: Socket;
+		server: Namespace;
+		chatRequestDto: ChatRequestDto;
+	}) {
+		const user = await this.roomRepository.getUserByClientId(client.id);
+		const users = await this.roomRepository.getUsersInRoom(user.roomUUID);
+
+		const targetIds: userUUID[] = this.getChatTargetIds({
+			others: users.filter((member) => user.uuid !== member.uuid),
+			chatRequestDto,
+		});
+
+		targetIds.forEach((targetId: userUUID) => {
+			server.to(targetId).emit(EVENT.RECEIVE_MESSAGE);
+		});
+
+		return {};
+	}
+
+	getChatTargetIds({
+		others,
+		chatRequestDto,
+	}: {
+		others: User[];
+		chatRequestDto: ChatRequestDto;
+	}) {
+		const { target, role, uuid } = chatRequestDto;
+
+		switch (target) {
+			case ChatTarget.EVERYONE:
+				return others.map((user) => user.uuid);
+			case ChatTarget.ROLE:
+				if (role) {
+					return others.filter((other) => other.role === role).map((user) => user.uuid);
+				}
+			case ChatTarget.DRIECT:
+				if (uuid) {
+					return [uuid];
+				}
+			default:
+				throw new WsException(EXCEPTION_MESSAGE.INVALID_CHAT_DATA);
+		}
+	}
+}
