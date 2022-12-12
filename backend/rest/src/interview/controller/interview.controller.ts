4a36213d84168ef1eec22189f0e017273291a1b2
punkryn <gns816715@gmail.com>
2022-12-12 23:08:02 +0900

    📖 docs: (BE) - interview controller swagger 추가

--- backend/rest/src/interview/controller/interview.controller.ts
+++ backend/rest/src/interview/controller/interview.controller.ts
@@ -17,12 +17,24 @@ import { DocsRequestDto } from '../dto/request-docs.dto';
 import { InterviewService } from '../service/interview.service';
 import { Request } from 'express';
 import { FeedbackRequestDto } from '../dto/request-feedback.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { DocsResponseDto } from '../dto/response-docs.dto';
 
+@ApiTags('interview')
 @UseGuards(JwtAuthGuard)
 @Controller('interview')
 export class InterviewController {
 	constructor(private readonly interviewService: InterviewService) {}
 
+	@ApiOperation({ summary: 'interview docs 생성하기' })
+	@ApiResponse({
+		status: HttpStatus.CREATED,
+		description: '생성 성공',
+	})
+	@ApiResponse({
+		status: HttpStatus.UNAUTHORIZED,
+		description: '생성 실패',
+	})
 	@Post('docs')
 	async createInterviewDocs(@Req() req: Request, @Body() docsRequestDto: DocsRequestDto) {
 		const payload = req.user as JwtPayload;
@@ -31,6 +43,15 @@ export class InterviewController {
 		return {};
 	}
 
+	@ApiOperation({ summary: 'feedback 생성하기' })
+	@ApiResponse({
+		status: HttpStatus.CREATED,
+		description: '생성 성공',
+	})
+	@ApiResponse({
+		status: HttpStatus.UNAUTHORIZED,
+		description: '생성 실패',
+	})
 	@Post('feedback')
 	async createFeedback(@Req() req: Request, @Body() feedbackRequestDto: FeedbackRequestDto) {
 		const payload = req.user as JwtPayload;
@@ -38,6 +59,16 @@ export class InterviewController {
 		return {};
 	}
 
+	@ApiOperation({ summary: 'docs UUID로 interview docs 가져오기' })
+	@ApiResponse({
+		status: HttpStatus.OK,
+		description: '성공',
+		type: DocsResponseDto,
+	})
+	@ApiResponse({
+		status: HttpStatus.UNAUTHORIZED,
+		description: '인증 실패',
+	})
 	@Get('docs/:docsUUID')
 	async getInterviewDocs(@Param('docsUUID') docsUUID: string) {
 		const interviewDocs = await this.interviewService.getInterviewDocs(docsUUID);
@@ -45,6 +76,15 @@ export class InterviewController {
 		return interviewDocs;
 	}
 
+	@ApiOperation({ summary: 'interview docs list 가져오기' })
+	@ApiResponse({
+		status: HttpStatus.OK,
+		description: '성공',
+	})
+	@ApiResponse({
+		status: HttpStatus.UNAUTHORIZED,
+		description: '인증 실패',
+	})
 	@Get('docs-list')
 	async getInterviewDocsList(@Req() req: Request, @Query('room-uuid') roomUUID: string) {
 		const payload = req.user as JwtPayload;
@@ -56,6 +96,15 @@ export class InterviewController {
 		return interviewDocsList;
 	}
 
+	@ApiOperation({ summary: 'interview docs 삭제하기' })
+	@ApiResponse({
+		status: HttpStatus.NO_CONTENT,
+		description: '삭제 성공',
+	})
+	@ApiResponse({
+		status: HttpStatus.UNAUTHORIZED,
+		description: '인증 실패',
+	})
 	@Delete('docs/:docsUUID')
 	@HttpCode(HttpStatus.NO_CONTENT)
 	async deleteInterviewDocs(@Req() req: Request, @Param('docsUUID') docsUUID: string) {
