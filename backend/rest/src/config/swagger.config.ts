4df305164fae010c98a417a030ab1ba767868f5a
punkryn <gns816715@gmail.com>
2022-12-12 23:07:19 +0900

    📖 docs: (BE) - swagger tag 추가

--- backend/rest/src/config/swagger.config.ts
+++ backend/rest/src/config/swagger.config.ts
@@ -5,6 +5,8 @@ const swaggerConfig = new DocumentBuilder()
 	.setTitle('인터페이스 API 문서')
 	.setDescription('Web14 - interface 프로젝트에서 사용하는 API를 작성한 문서입니다.')
 	.setVersion('0.0.1')
+	.addTag('auth')
+	.addTag('interview')
 	.addCookieAuth('accessToken', {
 		type: 'http',
 		in: 'header',
