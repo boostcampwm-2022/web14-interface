328039972534b85204e84c7bd8f53648ce3f2fee
Janghun Lee <bhwk2980@gmail.com>
2022-12-12 10:48:40 +0900

    💄 design: (FE) - 버튼 스타일링 적용

--- frontend/src/components/@shared/BottomBarButton/BottomBarButton.tsx
+++ frontend/src/components/@shared/BottomBarButton/BottomBarButton.tsx
@@ -1,5 +1,6 @@
+import { css } from '@emotion/react';
 import React from 'react';
-import { buttonStyle } from './Button.style';
+import { buttonStyle } from '../Button/Button.style';
 
 export interface buttonPropType {
 	children?: React.ReactNode | React.ReactNode[];
@@ -11,24 +12,27 @@ export interface buttonPropType {
 	iconColor?: boolean;
 	disabled?: boolean;
 	onClick?: React.MouseEventHandler<HTMLButtonElement>;
+	visibility?: 'visible' | 'hidden';
 }
 
-const Button = ({
+const BottomBarButtom = ({
 	children,
 	width,
-	size = 'medium',
-	style = 'contained',
-	color = 'primary',
+	size = 'large',
+	style = 'text',
+	color = 'secondary',
 	justifyContent = 'center',
 	iconColor = true,
 	disabled = false,
 	onClick,
+	visibility,
 }: buttonPropType) => {
 	return (
 		<button
-			css={(theme) =>
-				buttonStyle(theme, width, size, style, color, iconColor, justifyContent)
-			}
+			css={[
+				(theme) => buttonStyle(theme, width, size, style, color, iconColor, justifyContent),
+				bottomBarPadding(visibility),
+			]}
 			disabled={disabled}
 			onClick={onClick}
 		>
@@ -37,4 +41,9 @@ const Button = ({
 	);
 };
 
-export default Button;
+export default BottomBarButtom;
+
+const bottomBarPadding = (visibility) => css`
+	visibility: ${visibility};
+	padding: 8px 12px;
+`;
