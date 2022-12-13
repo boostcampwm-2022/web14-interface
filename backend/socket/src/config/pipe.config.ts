6793cf9fe0dc28e99f75186cb1d6f59b7a83edd8
AnJeongMin <ajhappy12@gmail.com>
2022-12-14 02:42:17 +0900

    ✨ feat: (BE) - socket server validation pipe 제대로 적용

--- backend/socket/src/config/pipe.config.ts
+++ backend/socket/src/config/pipe.config.ts
@@ -2,6 +2,7 @@ import { ValidationPipeOptions } from '@nestjs/common';
 
 export const pipeOptions: ValidationPipeOptions = {
 	transform: true,
+	whitelist: true,
 	forbidNonWhitelisted: true,
 	forbidUnknownValues: true,
 };
