9942a537edf56707a1c451a669f2ad52d04471cf
Janghun Lee <bhwk2980@gmail.com>
2022-12-14 11:42:49 +0900

    💄 design: (FE) - Interviewer 컴포넌트 가로 크기 증가

--- frontend/src/pages/Interviewer/Interviewer.style.tsx
+++ frontend/src/pages/Interviewer/Interviewer.style.tsx
@@ -5,10 +5,13 @@ export const interviewerWrapperStyle = (theme) => css`
 	width: 100%;
 	height: calc(100% - ${theme.bottomBarHeight});
 	background-color: ${theme.colors.tertiary};
+
+	padding: 56px;
 `;
 
 export const interviewerContainerStyle = (theme) => css`
 	${flexRow({ gap: '48px' })};
 
 	height: 100%;
+	color: ${theme.colors.white};
 `;
