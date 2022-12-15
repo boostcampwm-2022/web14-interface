ec630ef16f52f2d110f89d2edd26ecf20982680d
punkryn <gns816715@gmail.com>
2022-12-15 15:35:08 +0900

    🚨 test: (BE) - room repository 의존성 주입, 모킹, 방 만들기 테스트

--- backend/socket/src/room/service/connection/connection.service.spec.ts
+++ backend/socket/src/room/service/connection/connection.service.spec.ts
@@ -1,18 +1,48 @@
+import { ROOM_PHASE, ROOM_REPOSITORY_INTERFACE } from '@constant';
 import { Test, TestingModule } from '@nestjs/testing';
+import { MockRepository } from '@types';
+import { RoomRepository } from 'src/room/repository/room.repository';
 import { ConnectionService } from './connection.service';
 
+const roomUUID = 'testRoomUUID';
+
+const mockRoomRepository = () => ({
+	createRoom: jest.fn(() => {
+		return {
+			roomUUID: roomUUID,
+			phase: ROOM_PHASE.LOBBY,
+		};
+	}),
+});
+
 describe('ConnectionService', () => {
-	let service: ConnectionService;
+	let connectionService: ConnectionService;
+	let roomRepository: MockRepository<RoomRepository>;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [ConnectionService],
+			providers: [
+				ConnectionService,
+				{
+					provide: ROOM_REPOSITORY_INTERFACE,
+					useValue: mockRoomRepository(),
+				},
+			],
 		}).compile();
 
-		service = module.get<ConnectionService>(ConnectionService);
+		connectionService = module.get<ConnectionService>(ConnectionService);
+		roomRepository = module.get(ROOM_REPOSITORY_INTERFACE);
 	});
 
 	it('should be defined', () => {
-		expect(service).toBeDefined();
+		expect(connectionService).toBeDefined();
+		expect(roomRepository).toBeDefined();
+	});
+
+	it('방 만들기 테스트', async () => {
+		const { data } = await connectionService.createRoom();
+		const { uuid } = data;
+
+		expect(uuid).toBe(roomUUID);
 	});
 });
