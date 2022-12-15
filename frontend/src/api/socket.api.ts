cd3676c1ffba84d17179f35e6a8f7583b83e278c
Janghun Lee <bhwk2980@gmail.com>
2022-12-16 01:45:46 +0900

    ✨ feat: (FE) - socket error 시 toast 기능 추가

--- frontend/src/api/socket.api.ts
+++ frontend/src/api/socket.api.ts
@@ -1,4 +1,5 @@
 import { socket } from '@service/socket';
+import { getToatMessage } from '@utils/common.util';
 
 interface socketResponseType<T> {
 	success: boolean;
@@ -10,11 +11,10 @@ export const socketEmit = <T>(event, data?) => {
 	return new Promise<T>((resolve, reject) => {
 		socket.emit(event, data, ({ success, data, message }: socketResponseType<T>) => {
 			console.log('socket.api', success, data, message);
+
 			if (!success) {
-				//TODO 여기서 에러 처리
-				//모달을 띄우던 -> useModal
-				//NOT Found 같은 에러 페이지로 이동 하던. -> throw Error -> React Error Boundary
-				alert(message);
+				getToatMessage(message);
+
 				return reject({ success, message });
 			}
 
