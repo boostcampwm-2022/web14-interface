7ddd50a17bf2b28e8bc349f39741055fcfd0ac54
Janghun Lee <bhwk2980@gmail.com>
2022-12-16 05:38:29 +0900

    ✨ feat: (FE) - toast 컴포넌트 및 로직 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,12 @@ import { RecoilRoot } from 'recoil';
 import { Global, ThemeProvider } from '@emotion/react';
 import { BrowserRouter } from 'react-router-dom';
 
-import 'react-toastify/dist/ReactToastify.min.css';
-import { ToastContainer } from 'react-toastify';
-
 import RootRoutes from '@routes/RootRoutes';
 import globalStyle from '@styles/globalStyle';
 import theme from '@styles/theme';
 import ModalManager from '@components/ModalManager';
 import Loading from '@components/Loading/Loading';
+import ToastContainer from '@components/ToastManager';
 
 function App() {
 	return (
@@ -23,20 +21,9 @@ function App() {
 							<RootRoutes />
 							<ModalManager />
 						</Suspense>
-						<ToastContainer
-							position="bottom-center"
-							autoClose={5000}
-							hideProgressBar={false}
-							newestOnTop={false}
-							closeOnClick
-							rtl={false}
-							pauseOnFocusLoss
-							draggable
-							pauseOnHover
-							theme="colored"
-						/>
 					</div>
 					<div id="popup-root"></div>
+					<ToastContainer />
 				</ThemeProvider>
 			</BrowserRouter>
 		</RecoilRoot>
---
+++ frontend/src/store/toast.store.ts
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const toastState = atom({
+	key: 'toastState',
+	default: null,
+});
