3112d71987339bdfc8900c4ffd4f2eee5b005a89
Janghun Lee <bhwk2980@gmail.com>
2022-12-15 19:09:22 +0900

    🐋 chore: (FE) - 주석 제거

--- frontend/src/components/@modal/EnterRoomModal.tsx
+++ frontend/src/components/@modal/EnterRoomModal.tsx
@@ -39,15 +39,6 @@ const EnterRoomModal = () => {
 				roomUUID
 			);
 
-			// //TODO BE 대응 시 변경
-			// const newOthers = others.map((user) => {
-			// 	return { ...user, video: true, audio: false };
-			// });
-			// console.log('newOthers', newOthers);
-			// setOthers(newOthers);
-			// //TODO BE 대응 시 변경
-			// setMe({ ...me, audio: false });
-
 			setRoom(roomUUID);
 			setOthers(others);
 			setMe(me);
